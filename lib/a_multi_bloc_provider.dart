cf4f52e400f3ef6b0acb086b52e58a083949c28d
Tilek Budeshov <112117485+i3tika@users.noreply.github.com>
2023-10-04 10:59:54 +0600

    a_multi_bloc_provider - changes a_multi_bloc_provider - modified:   lib/a_multi_bloc_provider.dart

--- lib/a_multi_bloc_provider.dart
+++ lib/a_multi_bloc_provider.dart
@@ -7,18 +7,22 @@ class AMultiBlocProvider extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(providers: [
-      BlocProvider(
-        create: (context) => PostCubit(),
-      ),
-      BlocProvider(
-        create: (context) =>
-            RickAndMortyBloc(repo: RepositoryProvider.of<RickRepo>(context)),
-      ),
-      BlocProvider(
-        create: (context) =>
-            JsonPhdBBloc(repo: RepositoryProvider.of<JsonPHDRepo>(context)),
-      ),
-    ], child: app);
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => PostCubit(),
+        ),
+        BlocProvider(
+          create: (context) =>
+              RickAndMortyBloc(repo: RepositoryProvider.of<RickRepo>(context)),
+        ),
+        BlocProvider(
+          create: (context) =>
+              JsonPhdBBloc(repo: RepositoryProvider.of<JsonPHDRepo>(context)),
+        ),
+      ],
+      child: app,
+    );
   }
 }
+
