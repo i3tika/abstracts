11e59d759e6b94200aa4e376bc7b9841026c0e10
Tilek Budeshov <112117485+i3tika@users.noreply.github.com>
2023-11-24 12:20:33 +0600

    dev - test push request
    - more date
    - new file:   .vscode/extensions.json
    - new file:   .vscode/launch.json
    - modified:   android/app/src/main/AndroidManifest.xml
    - modified:   lib/app.dart
    - modified:   lib/core/ui/widgets/atextfield_widget.dart
    - new file:   lib/feature/auth_subabase/state/auth_supa/auth_supa_bloc.dart
    - new file:   lib/feature/auth_subabase/state/auth_supa/auth_supa_event.dart
    - new file:   lib/feature/auth_subabase/state/auth_supa/auth_supa_state.dart
    - new file:   lib/feature/example/data/models/response/test.dart
    - new file:   lib/feature/example/data/models/response/test.g.dart
    - new file:   lib/feature/form_key/form_key.dart
    - new file:   lib/feature/qr_code_scaner/_qr_code_scaner.dart
    - new file:   lib/feature/qr_code_scaner/data/_data.dart
    - new file:   lib/feature/qr_code_scaner/data/models/_models.dart
    - new file:   lib/feature/qr_code_scaner/data/repo/_repo.dart
    - new file:   lib/feature/qr_code_scaner/pages/_pages.dart
    - new file:   lib/feature/qr_code_scaner/pages/detector_painters.dart
    - new file:   lib/feature/qr_code_scaner/pages/qr_scanlit.dart
    - new file:   lib/feature/qr_code_scaner/state/_state.dart
    - new file:   lib/feature/qr_code_scaner/widgets/_widgets.dart
    - new file:   lib/feature/sliver/sliver_widget.dart
    - modified:   lib/feature/testings_api/pages/json_phd.dart
    - modified:   lib/generated/intl/messages_en.dart
    - modified:   lib/generated/l10n.dart
    - modified:   lib/l10n/intl_en.arb
    - modified:   lib/main.dart
    - modified:   pubspec.lock
    - modified:   pubspec.yaml

---
+++ lib/feature/task/gg/test_gg_state.dart
@@ -0,0 +1,10 @@
+part of 'test_gg_bloc.dart';
+
+abstract class TestGgState extends Equatable {
+  const TestGgState();
+}
+
+class TestGgInitial extends TestGgState {
+  @override
+  List<Object> get props => [];
+}
